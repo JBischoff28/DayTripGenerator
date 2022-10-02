9d66f70bd870267f8c1e806b378aea1ed736a5f5
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-10-01 22:26:03 -0400

    Created a function to randomly generate a destination from a list

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+
+def generate_random_destination ():
+    destination_list = ["Newport, RI", "Nashville, TN", "San Diego, CA", "Seattle, WA", "Chicago, IL", "The Bahamas", "Miami, FL", "The Grand Caynon", "Las Vegas, NE", "Scottsdale, AR", "Colorado Springs, CO", "Outter Banks, NC", "Charleston, SC"]
+    random_destination = random.choice(destination_list)
+    print(random_destination)
+
+def generate_random_restaurant ():
+    restaurant_list = ["The Brass", "O'Kelly's Bar & Grill", "Camille's", "Apple Bees", "Buffalo Wild Wings", "Los Paleminos", "Olive Garden", "Starlite", "Max & Emily's", "Jersey Mike's", "Freddie's", "IHOP", "CMU Dining Hall", "Menna's Joint", "Pizza Hut"]
+    random_restaurant = random.choice(restaurant_list)
+    print(random_restaurant)
+
+generate_random_destination()
+generate_random_restaurant()
